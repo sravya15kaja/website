d4fc3ef25f644bed20f712ae6908d0619dc9ab09
sravya15kaja <sravyakaja15@gmail.com>
2025-04-19 13:03:44 +0530

    web

---
+++ careers.js
@@ -0,0 +1,8 @@
+function Internship() {
+    const section = document.getElementById("section-2");
+    if (section.style.display === "none" || section.style.display === "") {
+        section.style.display = "block";
+    } else {
+        section.style.display = "none";
+    }
+}
